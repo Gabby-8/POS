34db3406b0d546c2230ceeeb269b50fe2c8e13e7
Gab <80657491+Gabby-8@users.noreply.github.com>
2026-04-24 00:57:32 +0800

    Add project files.

---
+++ POS.Domain/POSDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using POS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Domain
+{
+    public class POSDbContext : DbContext
+    {
+        public POSDbContext(DbContextOptions<POSDbContext> options) : base(options) { }
+
+        public DbSet<RoleCategoryModel> RoleCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.ConfigureWarnings(warnings =>
+            warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<RoleCategoryModel>(entity =>
+            {
+                entity.HasData(
+                    new RoleCategoryModel { ID = 1, RoleName = "Admin" },
+                    new RoleCategoryModel { ID = 2, RoleName = "User" }
+                    );
+            });
+        }
+    }
+}
---
+++ pos.client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+
+    return (
+        <div>
+            <h1 id="tableLabel">Product List</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+        </div>
+    );
+}
+
+export default App;
